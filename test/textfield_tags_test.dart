c6a8fc47f5ba1f95a1507d9a0bc12e32bbec746c
eyoeldefare <defareeyoel@gmail.com>
2020-10-15 10:43:54 -0400

    Add additional example demo

--- test/textfield_tags_test.dart
+++ test/textfield_tags_test.dart
@@ -1,7 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:textfield_tags/textfield_tags.dart';
-
 void main() {
   test('adds one to input values', () {
 //    final calculator = Calculator();
